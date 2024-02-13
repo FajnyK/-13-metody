2d1a27e29e86bbd75c97dbcff79cb67b1157a772
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2024-02-14 00:53:38 +0100

    Initial commit

---
+++ Kot.cs
@@ -0,0 +1,40 @@
+﻿namespace _13_metody
+{
+    internal class Kot
+    {
+        public string imie = "brak danych";
+        public int wiek;
+        public float waga;
+        public string kolorOczu;
+        public string kolorSiersci;
+
+        private int liczbaZlapanychMyszy;
+        private TimeSpan czasNaSnie;
+        private DateTime poczatekSnu;
+
+        public void PokazInformacje()
+        {
+            if (poczatekSnu != DateTime.MinValue)
+            {
+                czasNaSnie = ObliczCzasSnu();
+            }
+            Console.WriteLine($"imie: {imie} \nwiek: {wiek} \nwaga: {waga} " +
+                $"\nkolor oczu: {kolorOczu} \nkolor siersci: {kolorSiersci} " +
+                $"\nliczba zlapanych myszy: {liczbaZlapanychMyszy} \nczas na snie: {czasNaSnie}");
+        }
+
+        public void ZlapacMysz()
+        {
+            Console.WriteLine($"{imie}: mysz zlapana");
+            liczbaZlapanychMyszy++;
+        }
+
+        public void Spac()
+        {
+            Console.WriteLine($"{imie} spi");
+            poczatekSnu = DateTime.Now;
+        }
+        
+        private TimeSpan ObliczCzasSnu() => DateTime.Now - poczatekSnu;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using _13_metody;
+
+Kot bella = new Kot();
+bella.imie = "Bella";
+bella.waga = 6f;
+bella.wiek = 9;
+bella.kolorOczu = "Zolty";
+bella.kolorSiersci = "Prazkowany";
+
+string koniecProgramy = "koniec";
+string wartosc = "";
+
+while (wartosc != koniecProgramy)
+{
+    wartosc = Console.ReadLine();
+
+    switch (wartosc)
+    {
+        case "info":
+            bella.PokazInformacje();
+            break;
+        case "spac":
+            bella.Spac();
+            break;
+        case "mysz":
+            bella.ZlapacMysz();
+            break;
+        default: break;
+    }
+}
